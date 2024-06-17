f600a7c36fa5498027e348e147417dbda5038efa
HeShouYi_gs <863649918@qq.com>
2024-06-17 18:19:47 +0800

    造测试数据的小工具，生成100w假数据+清理数据库的分库表/修改

--- Others/database_functions/fake_data_mysql/api_supplementary_push.py
+++ Others/database_functions/fake_data_mysql/api_supplementary_push.py
@@ -10,12 +10,14 @@ from faker import Faker
 from datetime import datetime
 import random
 import uuid
+from tqdm import tqdm
+import time
 
 fake = Faker()
 
 # 连接数据库
 connection = pymysql.connect(
-    host='localhost',
+    host='192.168.21.249',
     port=5831,
     user='root',
     password='Keytop:wabjtam!',
@@ -34,7 +36,19 @@ try:
         """
 
         data = []
-        for _ in range(1000):
+        num_records = 1000000
+        batch_size = 1000
+
+        # 开始计时
+        start_time = time.time()
+        data_gen_time = 0
+        db_insert_time = 0
+
+        for _ in tqdm(range(num_records), desc="Inserting records"):
+
+            # 记录数据生成时间
+            gen_start_time = time.time()
+
             url = fake.url()
             header = fake.text(max_nb_chars=50)
             params = fake.text(max_nb_chars=50)
@@ -47,16 +61,34 @@ try:
                 url, header, params, reason, status, req_id, create_time
             ))
 
+            gen_end_time = time.time()
+            data_gen_time += (gen_end_time - gen_start_time)
             # 每1000条数据批量插入一次
-            if len(data) == 1000:
+            if len(data) == batch_size:
+                insert_start_time = time.time()
+
                 cursor.executemany(sql, data)
                 connection.commit()
+
+                insert_end_time = time.time()
+                db_insert_time += (insert_end_time - insert_start_time)
+
                 data = []
 
         # 插入剩余数据
         if data:
             cursor.executemany(sql, data)
             connection.commit()
+
+            insert_end_time = time.time()
+            db_insert_time += (insert_end_time - insert_start_time)
+
+        end_time = time.time()
+
+        print(f"数据生成时间: {data_gen_time: .2f}秒")
+        print(f"数据库插入时间: {db_insert_time: .2f}秒")
+        print(f"总时间: {end_time - start_time: .2f}秒")
+
 
 finally:
     connection.close()
--- Others/database_functions/fake_data_mysql/drop_tables.py
+++ Others/database_functions/fake_data_mysql/drop_tables.py
@@ -7,88 +7,98 @@
 # @description:
 import pymysql
 
-# 连接数据库
-connection = pymysql.connect(
-    host='192.168.21.249',
-    port=5831,
-    user='root',
-    password='Keytop:wabjtam!',
-    database='parking_guidance'
-)
+
+def clean_database_backup():
+    """
+    清理数据库归档之后的备份库，测试用
+    :return:
+    """
+
+    # 连接数据库
+    connection = pymysql.connect(
+        host='192.168.21.249',
+        port=5831,
+        user='root',
+        password='Keytop:wabjtam!',
+        database='parking_guidance'
+    )
+
+    try:
+        # b_car_in_out_record清理
+        with connection.cursor() as cursor:
+            # 查询所有以 b_car_in_out_record_202 开头的表名
+            cursor.execute("""
+                SELECT table_name 
+                FROM information_schema.tables 
+                WHERE table_schema = 'parking_guidance' 
+                  AND table_name LIKE 'b_car_in_out_record_202%'
+            """)
+            tables_record = cursor.fetchall()
+
+            # 生成并执行 DROP TABLE 语句
+            for table in tables_record:
+                drop_table_sql = f"DROP TABLE {table[0]}"
+                cursor.execute(drop_table_sql)
+        # 提交更改
+        connection.commit()
+
+        # b_car_in_out_record_area清理
+        with connection.cursor() as cursor:
+            # 查询所有以 b_car_in_out_record_area_202 开头的表名
+            cursor.execute("""
+                SELECT table_name 
+                FROM information_schema.tables 
+                WHERE table_schema = 'parking_guidance' 
+                  AND table_name LIKE 'b_car_in_out_record_area_202%'
+            """)
+            tables_record_area = cursor.fetchall()
+
+            # 生成并执行 DROP TABLE 语句
+            for table in tables_record_area:
+                drop_table_sql = f"DROP TABLE {table[0]}"
+                cursor.execute(drop_table_sql)
+        # 提交更改
+        connection.commit()
+
+        # api_supplementary_push清理
+        with connection.cursor() as cursor:
+            # 查询所有以 api_supplementary_push_202 开头的表名
+            cursor.execute("""
+                SELECT table_name 
+                FROM information_schema.tables 
+                WHERE table_schema = 'parking_guidance' 
+                  AND table_name LIKE 'api_supplementary_push_202%'
+            """)
+            tables_supplementary_push = cursor.fetchall()
+
+            # 生成并执行 DROP TABLE 语句
+            for table in tables_supplementary_push:
+                drop_table_sql = f"DROP TABLE {table[0]}"
+                cursor.execute(drop_table_sql)
+        # 提交更改
+        connection.commit()
+
+        # b_recognition_record清理
+        with connection.cursor() as cursor:
+            # 查询所有以 b_recognition_record_202 开头的表名
+            cursor.execute("""
+                SELECT table_name 
+                FROM information_schema.tables 
+                WHERE table_schema = 'parking_guidance' 
+                  AND table_name LIKE 'b_recognition_record_202%'
+            """)
+            tables_supplementary_push = cursor.fetchall()
+
+            # 生成并执行 DROP TABLE 语句
+            for table in tables_supplementary_push:
+                drop_table_sql = f"DROP TABLE {table[0]}"
+                cursor.execute(drop_table_sql)
+        # 提交更改
+        connection.commit()
+
+    finally:
+        connection.close()
 
 
-try:
-    # b_car_in_out_record清理
-    with connection.cursor() as cursor:
-        # 查询所有以 b_car_in_out_record_202 开头的表名
-        cursor.execute("""
-            SELECT table_name 
-            FROM information_schema.tables 
-            WHERE table_schema = 'parking_guidance' 
-              AND table_name LIKE 'b_car_in_out_record_202%'
-        """)
-        tables_record = cursor.fetchall()
-
-        # 生成并执行 DROP TABLE 语句
-        for table in tables_record:
-            drop_table_sql = f"DROP TABLE {table[0]}"
-            cursor.execute(drop_table_sql)
-    # 提交更改
-    connection.commit()
-
-    # b_car_in_out_record_area清理
-    with connection.cursor() as cursor:
-        # 查询所有以 b_car_in_out_record_area_202 开头的表名
-        cursor.execute("""
-            SELECT table_name 
-            FROM information_schema.tables 
-            WHERE table_schema = 'parking_guidance' 
-              AND table_name LIKE 'b_car_in_out_record_area_202%'
-        """)
-        tables_record_area = cursor.fetchall()
-
-        # 生成并执行 DROP TABLE 语句
-        for table in tables_record_area:
-            drop_table_sql = f"DROP TABLE {table[0]}"
-            cursor.execute(drop_table_sql)
-    # 提交更改
-    connection.commit()
-
-    # api_supplementary_push清理
-    with connection.cursor() as cursor:
-        # 查询所有以 api_supplementary_push_202 开头的表名
-        cursor.execute("""
-            SELECT table_name 
-            FROM information_schema.tables 
-            WHERE table_schema = 'parking_guidance' 
-              AND table_name LIKE 'api_supplementary_push_202%'
-        """)
-        tables_supplementary_push = cursor.fetchall()
-
-        # 生成并执行 DROP TABLE 语句
-        for table in tables_supplementary_push:
-            drop_table_sql = f"DROP TABLE {table[0]}"
-            cursor.execute(drop_table_sql)
-    # 提交更改
-    connection.commit()
-
-    # b_recognition_record清理
-    with connection.cursor() as cursor:
-        # 查询所有以 b_recognition_record_202 开头的表名
-        cursor.execute("""
-            SELECT table_name 
-            FROM information_schema.tables 
-            WHERE table_schema = 'parking_guidance' 
-              AND table_name LIKE 'b_recognition_record_202%'
-        """)
-        tables_supplementary_push = cursor.fetchall()
-
-        # 生成并执行 DROP TABLE 语句
-        for table in tables_supplementary_push:
-            drop_table_sql = f"DROP TABLE {table[0]}"
-            cursor.execute(drop_table_sql)
-    # 提交更改
-    connection.commit()
-
-finally:
-    connection.close()
+if __name__ == "__main__":
+    clean_database_backup()
