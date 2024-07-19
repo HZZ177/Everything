4e3de666bf7c5a16e3adae35afe0f10f56b88e17
HeShouYi_home <863649918@qq.com>
2024-07-20 02:39:38 +0800

    初始化locust练手项目

---
+++ Others/Locust/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/7/20 上午12:07
+# @Author  : Heshouyi
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @description:
---
+++ Others/Locust/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/7/20 上午12:07
+# @Author  : Heshouyi
+# @File    : main.py
+# @Software: PyCharm
+# @description:
+
+import locust
+
+
+class Test(locust.HttpUser):
+    host = "http://127.0.0.1:8000"
+    wait_time = locust.between(1, 2)
+
+    @locust.task
+    def test_api(self):
+        result = self.client.get("/api/v1/test/")
+        assert result.status_code == 200
