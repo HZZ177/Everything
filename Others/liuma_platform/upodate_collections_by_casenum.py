421ecc1d7aa3bf2300bb0a37d9111bb6c751aee0
HeShouYi_gy <863649918@qq.com>
2024-09-25 21:55:52 +0800

    更新liuma脚本

--- Others/liuma_platform/upodate_collections_by_casenum.py
+++ Others/liuma_platform/upodate_collections_by_casenum.py
@@ -18,52 +18,35 @@ class App:
     def __init__(self):
 
         # 数据库配置
-        self.db_host = '101.227.53.213'
-        self.db_port = 3306
-        self.db_user = 'root'
-        self.db_password = 'K#2dOho@Dgts'
-        self.db_database = 'liuma'
+        self.db_config = {
+            'host': '101.227.53.213',
+            'user': 'root',
+            'password': 'K#2dOho@Dgts',
+            'port': 3306,
+            'database': 'liuma'
+        }
+        self.conn = self.connect_db()
 
-        self.conn = None     # 初始化数据库连接
-
+    def connect_db(self):
         try:
-            # 连接到数据库
-            self.conn = pymysql.connect(
-                host=self.db_host,
-                user=self.db_user,
-                password=self.db_password,
-                port=self.db_port,
-                database=self.db_database
-            )
+            return pymysql.connect(**self.db_config)
         except Exception as e:
             print(f"连接数据库失败，错误信息: {e}")
-            return
+            return None
 
     def select_case_info_by_num(self, case_num_list, case_id_list, case_name_list, module_name_list):
-        for case_num in case_num_list:
-            cursor = self.conn.cursor()
-            cursor.execute("SELECT id,name,module_id FROM `case` WHERE num = %s", (case_num,))
-            result = cursor.fetchone()
-            if result is not None:
-                # 通过module_id查询对应module名称
-                cursor.execute("SELECT name FROM `case_module` WHERE id = %s", (result[2],))
-                module_name = cursor.fetchone()[0]
-                case_id_list.append(result[0])
-                case_name_list.append(result[1])
-                module_name_list.append(module_name)
+        with self.conn.cursor() as cursor:
+            for case_num in case_num_list:
+                cursor.execute("SELECT id,name,module_id FROM `case` WHERE num = %s", (case_num,))
+                result = cursor.fetchone()
+                if result is not None:
+                    # 通过module_id查询对应module名称
+                    cursor.execute("SELECT name FROM `case_module` WHERE id = %s", (result[2],))
+                    module_name = cursor.fetchone()[0]
+                    case_id_list.append(result[0])
+                    case_name_list.append(result[1])
+                    module_name_list.append(module_name)
         print(case_id_list, case_name_list, module_name_list)
-
-    def select_module_name_by_id(self, ):
-        for case_num in case_num_list:
-            cursor = self.conn.cursor()
-            cursor.execute("SELECT id,name,module_id FROM `case` WHERE num = %s", (case_num,))
-            result = cursor.fetchone()
-            if result is not None:
-                case_id_list.append(result[0])
-                case_name_list.append(result[1])
-
-        print(case_id_list, case_name_list)
-
 
     @staticmethod
     def get_collection_detail_byid(token, collection_id):
@@ -80,7 +63,6 @@ class App:
         except ValueError:
             print("响应不是有效的JSON")
             return result
-
 
     @staticmethod
     def save_collection_with_new_cases(token, collection_id, collection_case_info, case_id_list, case_name_list, module_name_list):
@@ -108,8 +90,7 @@ class App:
 
         print(collection_case_info)
 
-        # requests.post(url=url, headers=headers, json=collection_case_info)
-        pass
+        # result = requests.post(url=url, headers=headers, json=collection_case_info)
 
 
 if __name__ == '__main__':
@@ -118,11 +99,12 @@ if __name__ == '__main__':
         "TQwM2MtYTMyMS1hMjVlYzUyYzE0MzMiLCJleHAiOjE3MjcyODI5OTIsImlhdCI6MTcyNzI1NDE5MiwiYWNjb3VudCI6Imhlc2hvdXlpIiwidXN"
         "lcm5hbWUiOiLkvZXlrojkuIAifQ.vmPgVcl8OYgM4n_5prmvcaHfX-XbtaEc7xyNhOq3a9M"
     )
-    case_num_list = [10038, 10004]  # 用例序号
+
+    case_num_list = [10014, 10029, 10038, 10029]  # 用例序号
     case_id_list = []       # 查出的用例主键id
     case_name_list = []       # 查出的用例名称
     module_name_list = []     # 对应模块id
-    collection_id = '54a2c895-9b0e-4d13-923e-2078147a222c'  # 集合id
+    collection_id = '1e5fb29a-d5c7-42ff-9b3f-0db853cd6ee7'  # 目标集合id
 
     app = App()
     app.select_case_info_by_num(case_num_list, case_id_list, case_name_list, module_name_list)
